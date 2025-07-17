8aa326ee03316a817afd9a8416097f229a438b8a
kalaikv <148685819+kalaikv33@users.noreply.github.com>
2025-07-17 18:57:20 +0530

    Update atm.c functions

--- C-practicefiles/atm.c
+++ C-practicefiles/atm.c
@@ -8,14 +8,16 @@
 #include<stdio.h>
 #include"string.h"
 
-int credit();
+int init_bank();
+unsigned int credit();
 int debit();
 
-static int avail_bal;
+static int avail_bal=200;
+unsigned int i=1;
 
 int main()
 {
-	unsigned const int acc_num=220044,i,menu;
+	unsigned const int acc_num=220044;
 	
 	printf("                              *** WELCOME ***                  ");
 	printf("\n                              TO ATM MACHINE                   ");
@@ -25,28 +27,47 @@ int main()
 	do{
 	   printf("\n\n\nEnter your Account number : ");
 	   scanf("%d",&acc_num);
+	   i++;
       }
-
-    while(i>=2);
+      
+    while(i<2);
     {
-	if(acc_num == 220044)
-	{
-		printf("\n                        Welcome to the ATM                  ");
-		printf("\n\nAccount Holder's name   : kabir");
-		printf("\nMobile Number           : 1234567891");
-		printf("\nAvailable Balance       : %d", avail_bal);
-		printf("\nBranch Name             : IDFC Bank rajouri garden ");
+	  if(acc_num == 220044)
+    	{
+		  printf("\n                        Welcome to the ATM                  ");
+		  printf("\n\nAccount Holder's name   : kabir");
+	      printf("\nMobile Number           : 1234567891");
+		  printf("\nAvailable Balance       : %d", avail_bal);
+		  printf("\nBranch Name             : IDFC Bank rajouri garden ");
 		
-		printf("\n\n\n                      SELECT                            ");
+		
+		     init_bank();
+	    }
+	
+	else
+		printf("\n\nEnter Account number is invalid !!!");
+    }
+	
+}
+
+int init_bank()
+{
+	unsigned int j;
+	char menu;
+	
+	printf("\n\n\n                      SELECT                            ");
 		printf("\n                   1. Credit                                ");
 				printf("\n                   2. Debit                                ");
 						printf("\n                   3. Available Balance                                ");
 								printf("\n                   4. Mini Balance                                ");
+							        	printf("\n                   5. EXIT                                ");
 								
 		printf("\n*********************************************************\n");
-		scanf("%s",&menu);
-								
-		switch (menu)
+		scanf("%c",&menu);
+		
+	  for(j=1;j<10;j++)
+	  {	
+		switch(menu)
 		{
 		  case '1':
 		  printf("\ncurrent page : credit ");
@@ -55,12 +76,12 @@ int main()
 		  
 		  case '2':
 		  printf("\ncurrent page : Debit ");
-		  //credit();	
+		  debit();	
 		  break;
 		  
 		  case '3':
 		  printf("\ncurrent page : Available Balance ");
-		  //credit();	
+		  printf("\n\nAvailable Balance : %d", avail_bal);	
 		  break;
 		  
 		  case '4':
@@ -68,21 +89,52 @@ int main()
 		  //credit();	
 		  break;
 		  
+		  case '5':
+		  printf("\ncurrent page : EXIT ");
+		   j=11;
+		  printf("\n\n\n       Thanks for choosing us !!!                                   ");
+		  break;
+		  
 		  default:
-		  	printf("\n\n\n       Thanks for choosing us !!!                                   ");
+		  printf("\n\n\n       Thanks for choosing us !!!                                   ");
 		}
-	}
-	
-	else
-		printf("\n\nEnter Account number is invalid !!!");
-    }
-	
+	  }
 }
-int credit()
+unsigned int credit()
 {
 	unsigned int credit_amt;
 	printf("\n\n\nEnter the amount to credit on account :  ");
 	scanf("%d",&credit_amt);
-	credit_amt+=avail_bal;
+	avail_bal+=credit_amt;
 	printf("\nCredited amount in account : %d", credit_amt);
+	printf("\nCurrent Balance in account : %d", avail_bal);
 }
+int debit()
+{
+	int debit_amt;
+	printf("\n\n\nEnter the amount to debit on account :  ");
+	scanf("%d",&debit_amt);
+	avail_bal-=debit_amt;
+	printf("\nDebited amount in account : %d", debit_amt);
+	printf("\nCurrent Balance in account : %d", avail_bal);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
