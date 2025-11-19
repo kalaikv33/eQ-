9bd1af97fbe7e52b1f12e1e4b84e156c82065f1b
kalaikv33 <kevinkv33vap@gmail.com>
2025-11-19 19:48:28 +0530

    BY using struct three example have worked

--- C-practicefiles/structure01.c
+++ C-practicefiles/structure01.c
@@ -3,12 +3,16 @@
    Date    : 19.11.2025
    Time    : 13:17 PM
 
-    used cases : functions,structure,array,typedef,#define
-    Need to use: pointer,
+    used cases : functions,structure,array,typedef,pointer
+    Need to use: ?
 */
+
+// By default compiler will process the memory size of member inside the structure : structure padding
+// To avoid padding ~ packing was used, it defines accurate memory size to the member : structure packing >> #pragma pack(1) used in header
 
 #include <stdio.h>
 #include <string.h>
+// #pragma pack(1)
 
 struct sensordata
 {
@@ -17,19 +21,42 @@ struct sensordata
     float sop_volt;
 } s;
 
+typedef struct unitval
+{ // unitval was last used member name
+    int n;
+} val; // while typedef here need to declare allias name for member
+
+struct value
+{
+    char name[10];
+    int number;
+    float value;
+};
+
 int main()
 {
     // struct sensordata s;
-    printf("Enter the sensor name : ");
-    scanf("%s", &s.sname);
-    printf("\nEnter the sensor type : ");
-    scanf("%s", &s.stype);
-    printf("\nEnter the sensor operating volatge : ");
-    scanf("%f", &s.sop_volt);
+    /* printf("\nEnter the sensor name : ");  scanf("%s", &s.sname);
+     printf("\nEnter the sensor type : ");  scanf("%s", &s.stype);
+     printf("\nEnter the sensor operating volatge : ");  scanf("%f", &s.sop_volt);
 
-    printf("\n***************************************************");
+     printf("\n***************************************************");
 
-    printf("\nsensor name    : %s", s.sname);
-    printf("\nsensor type    : %s", s.stype);
-    printf("\nsensor op volt : %.02f", s.sop_volt);
+     printf("\nsensor name    : %s", s.sname);
+     printf("\nsensor type    : %s", s.stype);
+     printf("\nsensor op volt : %.02f", s.sop_volt);
+
+     //****************************************************************************************************************************************
+
+     //typedef can init like this
+     val q = {22}; //val = datatype, q = declared member
+     printf("%d",q.n);
+
+     //****************************************************************************************************************************************
+     */
+    struct value n1 = {"datalogic", 3, 3.5};          // values assigned for struct variables statically
+    struct value *ptr = &n1;                          // pointer variable declared & make = to the address of struct variable
+    printf("\nEnter the value name : %s", ptr->name); // asusally we can get output like pointer using -> operator with member variable
+    printf("\nEnter the value number : %d", ptr->number);
+    printf("\nEnter the value  : %.2f", ptr->value);
 }
